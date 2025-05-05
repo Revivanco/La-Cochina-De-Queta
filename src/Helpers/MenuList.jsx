641d3e10e1c07267c7107d5d09f77a58e49b297f
Regina Vivanco Lemus <revilem1331@gmail.com>
2025-05-04 22:51:00 -0600

    menu and menu list were made.

---
+++ src/Helpers/MenuList.jsx
@@ -0,0 +1,48 @@
+import "../assets/ateConQueso.jpg";
+import "../assets/empanadasDeCochinita.jpg";
+import "../assets/salsaHbanero.jpg";
+import "../assets/tacosCohinita.jpg";
+import "../assets/kgDeCochinita.jpg";
+import "../assets/tortaCochinita.jpg"
+
+
+
+
+export const MenuList = [
+    {
+        name: " 1 Kg de Cochinita Pibil",
+        image: kgDeCochinita,
+        price: "$470"
+    },
+
+    {
+        name: "Torta",
+        image: tortaCohinita,
+        price: "$80"
+    },
+
+    {
+        name: "Tacos",
+        image: tacosCochinita,
+        price: "$45 c/u"
+    },
+
+    {
+        name: "Empanadas",
+        image: empanadasDeCochinita,
+        price: "$260 10pz"
+    },
+
+    {
+        name: "Salsa de chile habanero",
+        image: salsaHabanero,
+        price: "$70 50ml"
+    },
+
+    {
+        name: "Ate con queso",
+        image: ateConQueso,
+        price: "$90"
+    },
+  
+]
---
+++ src/pages/Menu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import {} from "../Helpers/MenuList"
+
+
+const Menu = () => {
+    return(
+        <div className="menu">
+            <h1 className="menuTilte">Nuestro Menu</h1>
+            <div className="menuList"></div>
+        </div>
+    )
+}
+
+export default Menu
