2e962fe779e4e306ff23c2161e366847a4b1caf5
Regina Vivanco Lemus <revilem1331@gmail.com>
2025-08-11 13:47:55 -0600

    Se modofico footer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,22 +1,38 @@
-
 import React from "react";
 import fotoQueta from "../assets/fotoQueta.png";
 
 const About = () => {
-    return (
-        <div className="About" style={{ width: "100%", textAlign: "center" }}>
-            <img
-                src={fotoQueta}
-                alt="Foto de Queta"
-                style={{
-                    width: "100%",
-                    height: "auto",
-                    maxHeight: "100vh",
-                    objectFit: "cover",
-                }}
-            />
-        </div>
-    );
+  return (
+    <div className="About">
+      <div
+        className="AboutTop"
+        style={{
+          backgroundImage: `url(${fotoQueta})`,
+          display: "flex",
+          alignItems: "center",
+          backgroundSize: "cover",
+          backgroundPosition: "center 40%",
+          backgroundRepeat: "no-repeat",
+          width: "100%",
+          height: "100vh",
+          minHeight: "400px",
+          maxHeight: "100vh",
+        }}
+      ></div>
+
+      <style>
+        {`
+          @media (max-width: 600px) {
+            .AboutTop {
+              height: 60vh !important;
+              background-position: center center !important;
+              background-size: contain !important;
+            }
+          }
+        `}
+      </style>
+    </div>
+  );
 };
 
 export default About;
